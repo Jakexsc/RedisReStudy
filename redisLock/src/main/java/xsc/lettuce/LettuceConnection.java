5d61874ae8bfda846983c32b26332c5c40e5290f
Jakexsc <649199985@qq.com>
2020-06-18 20:17:08 +0800

    Redis布隆过滤器

---
+++ redisLock/src/main/java/xsc/lettuce/LettuceConnection.java
@@ -0,0 +1,15 @@
+package xsc.lettuce;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
+public class LettuceConnection {
+    public static void main(String[] args) {
+        RedisClient lettuce = RedisClient.create("redis://123@192.168.37.128");
+        StatefulRedisConnection<String, String> connection = lettuce.connect();
+        RedisCommands<String, String> sync = connection.sync();
+        sync.set("name", "xsc");
+        System.out.println(sync.get("name"));
+    }
+}
